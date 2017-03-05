c3f88cc1a5ac5b0f2dedd094c9523028b0e1aaf9
Brian Broll <brian.broll@gmail.com>
2017-03-05 10:31:35 -0600

    Get started btn scroll to section

---
+++ www/main.js
@@ -0,0 +1,25 @@
+$(document).ready(function() {
+    $('.materialboxed').materialbox();
+
+    // get started button scrolling
+    (function() {
+        var btn = document.getElementById('get-started-btn'),
+            tgtSection = document.getElementById('quick-start');
+
+        btn.onclick = function() {
+            $(document.body).animate({
+                'scrollTop':   $('#quick-start').offset().top
+            }, 500); 
+        };
+    })();
+
+    // Get the latest version name and update the main link
+    $.ajax({
+        url: 'https://api.github.com/repos/deepforge-dev/deepforge/releases/latest'
+    }).done(function(data) {
+        var releaseAnchor = $('#latest-version');
+        if (data) {
+            releaseAnchor.text(data.name);
+        }
+    });
+});
